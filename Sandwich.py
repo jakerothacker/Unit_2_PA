cb1a108544ae01550455b304090116e31f082adf
Jake Rothacker <jakerothacker@gmail.com>
2026-06-10 16:20:54 -0400

    Changed Sandwich.py so finished list fills up

--- Sandwich.py
+++ Sandwich.py
@@ -7,7 +7,9 @@ count = 0
 
 while count < num_orders:
     print(sandwich_orders[count])
+    finished_sandwiches.append(sandwich_orders[count])
     count += 1 
 
 
 print("End is reached")
+print("Finished sandwiches:", finished_sandwiches)
